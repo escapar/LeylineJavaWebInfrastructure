98b603ae6318b9520007a4fa4abc74c74719ee90
ThrowableFactory <ThrowableFactory@foxmail.com>
2016-05-29 16:45:58 +0800

    init

---
+++ src/main/java/moe/src/leyline/LeylineApplication.java
@@ -0,0 +1,12 @@
+package moe.src.leyline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeylineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeylineApplication.class, args);
+	}
+}
---
+++ src/test/java/moe/src/leyline/LeylineApplicationTests.java
@@ -0,0 +1,16 @@
+package moe.src.leyline;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LeylineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
