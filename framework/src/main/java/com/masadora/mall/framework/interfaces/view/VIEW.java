6e8e12aee3525a780e2e6b5d8e13438b900ecee5
POJO <hzjdev@foxmail.com>
2016-06-07 15:03:32 +0800

    Integrate QueryDSL

--- framework/src/main/java/com/masadora/mall/framework/interfaces/view/VIEW.java
+++
@@ -1,9 +0,0 @@
-package com.masadora.mall.framework.interfaces.view;
-
-/**
- * Created by bytenoob on 5/29/16.
- */
-public class VIEW {
-    public interface LIST {}
-    public interface DETAIL extends LIST {}
-}
