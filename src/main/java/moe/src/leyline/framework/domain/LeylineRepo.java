8eb4285aac4ece507ac842d12568428c6b493151
ThrowableFactory <ThrowableFactory@foxmail.com>
2016-06-11 13:58:41 +0800

    Added query examples for querydsl , nativeSQL and HQL for limited fields and Java Bean

--- src/main/java/moe/src/leyline/framework/domain/LeylineRepo.java
+++ src/main/java/moe/src/leyline/framework/domain/LeylineRepo.java
@@ -1,5 +1,9 @@
 package moe.src.leyline.framework.domain;
 
+import java.util.stream.Stream;
+
+import com.querydsl.core.types.Predicate;
+
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
@@ -7,6 +11,5 @@ import org.springframework.data.repository.PagingAndSortingRepository;
  * Created by POJO on 6/2/16.
  */
 public interface LeylineRepo<T extends LeylineDO> extends PagingAndSortingRepository<T, Long>, QueryDslPredicateExecutor {
-
 
 }
