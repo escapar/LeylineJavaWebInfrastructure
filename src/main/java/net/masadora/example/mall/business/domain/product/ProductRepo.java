cb7967bbcb52b502fdff37396a7839a91a58e187
ThrowableFactory <ThrowableFactory@foxmail.com>
2016-06-17 21:47:03 +0800

    import

--- src/main/java/net/masadora/example/mall/business/domain/product/ProductRepo.java
+++ src/main/java/net/masadora/example/mall/business/domain/product/ProductRepo.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import moe.src.leyline.framework.domain.LeylineRepo;
-import net.masadora.mall.business.domain.product.QProduct;
 
 /**
  * Created by POJO on 6/3/16.
