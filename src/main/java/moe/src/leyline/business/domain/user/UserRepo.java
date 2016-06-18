f3673a054901474a9ceacd1635a0a56e3f0c6577
ThrowableFactory <ThrowableFactory@foxmail.com>
2016-06-18 16:29:42 +0800

    some customizations

--- src/main/java/moe/src/leyline/business/domain/user/UserRepo.java
+++ src/main/java/moe/src/leyline/business/domain/user/UserRepo.java
@@ -9,15 +9,15 @@ import moe.src.leyline.framework.domain.user.LeylineUserRepo;
  * Created by POJO on 6/8/16.
  */
 @Repository
-public interface UserRepo extends LeylineUserRepo<User> {
-    User findByNameAndPassword(String name , String password);
+public interface UserRepo extends LeylineUserRepo<DomainUser> {
+    DomainUser findByNameAndPassword(String name , String password);
 
     default Boolean authOK(String name,String password){
-        User u = findByNameEquals(name);
+        DomainUser u = findByNameEquals(name);
         return u!=null && BCrypt.checkpw(password, u.getPassword());
     }
 
-    default User checkAndGet(String name,String password){
+    default DomainUser checkAndGet(String name,String password){
         return authOK(name,password)? findByNameEquals(name) : null;
     }
 }
